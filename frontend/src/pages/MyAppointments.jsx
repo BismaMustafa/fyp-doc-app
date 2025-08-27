2fe0b8fd8bca07902a4c0ed5f6f11a295495c863
Bisma Mustafa <bsmalik8866@gmail.com>
2025-08-27 19:34:42 +0500

    doc-app

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import { useEffect } from 'react';
+import { loadStripe } from '@stripe/stripe-js'
 
 function MyAppointments() {
   const {backendUrl,token,getDoctorsData} = useContext(AppContext);
@@ -48,6 +49,7 @@ const slotDateFormat =  (slotDate) =>{
       
     }
   }
+    
   
   useEffect(()=>{
     if(token){
@@ -74,7 +76,7 @@ const slotDateFormat =  (slotDate) =>{
               </div>
               <div> </div>
               <div className='flex flex-col gap-2 justify-end'>
-                {!item.cancelled && !item.isCompleted && <button  className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border border-gray-300 rounded cursor-pointer hover:bg-blue-400 hover:text-white transition-all duration-300'>Pay Online</button>}
+                {/* {!item.cancelled && !item.isCompleted && <button   className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border border-gray-300 rounded cursor-pointer hover:bg-blue-400 hover:text-white transition-all duration-300'>Pay Online</button>} */}
                 {!item.cancelled && !item.isCompleted && <button onClick={()=>cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border border-gray-300 rounded cursor-pointer hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel appointment</button>}
                 {item.cancelled && !item.isCompleted && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500 cursor-pointer'>Appointment Cancelled</button>}
                 {item.isCompleted && <button className='sm:min-w-48 py-2  border border-green-500 rounded text-green-500'>Completed</button>}
